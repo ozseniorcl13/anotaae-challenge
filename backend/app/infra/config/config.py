99993cfb48c4dd95400e0608e2c291747a3e8eaf
Oziel Senior <seniorcl13@gmail.com>
2024-08-09 00:10:04 -0400

    devop(api): integrate with compose

--- backend/app/infra/config/config.py
+++ backend/app/infra/config/config.py
@@ -12,14 +12,13 @@ class Config(BaseSettings):
 
     BACKEND_CORS_SETTINGS: List[Union[AnyHttpUrl, str]] = ["*"]
     USE_DEBUG: bool = os.environ.get("USE_DEBUG", default=False)
-    HOST: str = os.environ.get("HOST", default="0.0.0.0")
-    PORT: int = int(os.environ.get("PORT", default=4000))
+    HOST: str = os.environ.get("BACKEND_HOST", default="0.0.0.0")
+    PORT: int = int(os.environ.get("BACKEND_PORT", default=4000))
 
-    DB_HOST: str = os.environ.get("DB_HOST")
+    DB_USER: str = os.environ.get("DB_USERNAME")
+    DB_PWD: str = os.environ.get("DB_PASSWORD")
+    DB_URL: str = os.environ.get("DB_URL")
     DB_NAME: str = os.environ.get("DB_NAME")
-    DB_USER: str = os.environ.get("DB_USER")
-    DB_PWD: str = os.environ.get("DB_PWD")
-    DB_PORT: int = int(os.environ.get("DB_PORT", default=3306))
 
 
 config = Config()
