bfb43a702dd7f9cbf4685e4e04210ae833f52db6
Oziel Senior <oziel.cm@sidi.org.br>
2024-08-18 23:24:28 -0400

    devop(api): add debug mode

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
 echo 'Running server...'
-python main.py
+
+echo "$ENV"
+
+if [ "$ENV" = "dev" ]; then
+    echo 'Dev mod'
+    python3 -m debugpy --listen 0.0.0.0:9999 main.py --reload
+else
+    echo 'Prod mod'
+    python3 main.py
+fi
